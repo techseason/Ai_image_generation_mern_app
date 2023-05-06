39fbdbd020bf578d75f906da370d8c41f8a3bf49
techseason <amanchaudharykumar@gmail.com>
2023-05-06 10:07:14 +0530

    commit

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-2-0-ahw8.onrender.com/api/v1/post', {
+      const response = await fetch('https://localhost:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
